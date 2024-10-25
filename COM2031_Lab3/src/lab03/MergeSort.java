f1462f0319361c470c75e1acd355583792dbd5fe
reachaki <akshyatdali@gmail.com>
2024-10-25 16:11:31 +0100

    lab3

--- COM2031_Lab3/src/lab03/MergeSort.java
+++ COM2031_Lab3/src/lab03/MergeSort.java
@@ -1,130 +1,107 @@
 package lab03;
 
 import java.util.ArrayList;
-
-/**
- * MergeSort
- * 
- * COM2031: Divide and Conquer Labs
- * Steve Schneider
- */
-
-
 import java.util.Arrays;
 import java.util.Random;
 
 public class MergeSort {
 
-	/**
-	 * 
-	 * @param arr an array of integers
-	 * @return  array of the integers n arr in sorted order
-	 */
-	
 	public static int[] mergeSort(int[] arr) {
-		// CALL TO RECURSIVE DIVIDE AND CONQUER METHOD
-		// TODO
-		return arr;  // replace this with the correct return value
+		return dac_mergeSort(arr, 0, arr.length);
 	}
-	
-	/**
-	 * 
-	 * @param arr    array of integers
-	 * @param start  an index of arr
-	 * @param end    index such that end-1 is an index of arr greater than or equal to start, 
-	 * @return       array of the integers in arr where the elements are sorted between start (inclusive) and end (exclusive)
-	 */
-	
+
 	public static int[] dac_mergeSort(int[] arr, int start, int end) {
-		// BASE CASE
-		if // TODO
-		else { // DIVIDE 
-			//TODO
-			//
-			// COMBINE
-			return merge(dac_mergeSort(arr, start, mid),dac_mergeSort(arr, mid, end));
-	}
-	}
-	
-	/**
-	 * 
-	 * @param arr1  sorted list of integers
-	 * @param arr2  sorted list of integers
-	 * @return      sorted list of all the integers in arr1 and arr2
-	 */
-	
-	public static int[] merge(int[] arr1, int[] arr2) {
-		// COMBINE
-		// TODO
-		return arr;
+		// BASE CASE: If the subarray has one or no elements
+		if (end - start <= 1) {
+			return Arrays.copyOfRange(arr, start, end);
+		} else {
+			// DIVIDE: Find the midpoint
+			int mid = (start + end) / 2;
+
+			// CONQUER: Recursively sort both halves
+			int[] leftSorted = dac_mergeSort(arr, start, mid);
+			int[] rightSorted = dac_mergeSort(arr, mid, end);
+
+			// COMBINE: Merge the two sorted halves
+			return merge(leftSorted, rightSorted);
 		}
-	
-/**********************
- * 
- * Utilities for testing
- * 
- */
-	
+	}
+
+	public static int[] merge(int[] arr1, int[] arr2) {
+		int[] merged = new int[arr1.length + arr2.length];
+		int i = 0, j = 0, k = 0;
+
+		// Merge the two sorted arrays
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i] <= arr2[j]) {
+				merged[k++] = arr1[i++];
+			} else {
+				merged[k++] = arr2[j++];
+			}
+		}
+
+		// If there are remaining elements in arr1
+		while (i < arr1.length) {
+			merged[k++] = arr1[i++];
+		}
+
+		// If there are remaining elements in arr2
+		while (j < arr2.length) {
+			merged[k++] = arr2[j++];
+		}
+
+		return merged;
+	}
+
 	public static void printArray(int[] a) {
 		System.out.print("{");
-		for(int i = 0; i< a.length-1; i++) {
-			System.out.print(a[i] + ", ");			
+		for (int i = 0; i < a.length - 1; i++) {
+			System.out.print(a[i] + ", ");
 		}
-			if(a.length>0) {
-				System.out.print(a[a.length - 1]);
-		
-			}
+		if (a.length > 0) {
+			System.out.print(a[a.length - 1]);
+		}
 		System.out.print("}");
 	}
-		
 
-	
-	public static void testMergeSort(String name, int[] test, int[] expected){
-	    int[] test2;
-	    test2 = mergeSort(test);
-	    if (Arrays.equals(test2, expected)){
-	    	System.out.println("  Pass:   "+name);
-	    }
-	    else {
-	    	System.out.println("*********************");	    	
-	    	System.out.println("  Fail:   "+name);
-	    	System.out.print("  Input:    ");
-	    	printArray(test);
-	    	System.out.println();
-	    	System.out.print("  Expected: ");
-	    	printArray(expected);
-	    	System.out.println();
-	    	System.out.print("  Actual:   ");
-	    	printArray(test2);
-	    	System.out.println();	    	
-	    	System.out.println("*********************");
-	    }
-//	    System.out.print("\n");
+	public static void testMergeSort(String name, int[] test, int[] expected) {
+		int[] test2 = mergeSort(test);
+		if (Arrays.equals(test2, expected)) {
+			System.out.println("  Pass:   " + name);
+		} else {
+			System.out.println("*********************");
+			System.out.println("  Fail:   " + name);
+			System.out.print("  Input:    ");
+			printArray(test);
+			System.out.println();
+			System.out.print("  Expected: ");
+			printArray(expected);
+			System.out.println();
+			System.out.print("  Actual:   ");
+			printArray(test2);
+			System.out.println();
+			System.out.println("*********************");
+		}
 	}
-	
-	
-	public static void main(String[] args) {
 
-		// allocate two arrays
-		int[] s1 = {2, 1, 4, 3, 6, 5};
-		int[] t1 = {1, 2, 3, 4, 5, 6};
+	public static void main(String[] args) {
+		int[] s1 = { 2, 1, 4, 3, 6, 5 };
+		int[] t1 = { 1, 2, 3, 4, 5, 6 };
 		testMergeSort("test1", s1, t1);
-		int[] s2 = {8,6,8,6,4,4,2};
-		int[] t2 = {2,4,4,6,6,8,8};
+		int[] s2 = { 8, 6, 8, 6, 4, 4, 2 };
+		int[] t2 = { 2, 4, 4, 6, 6, 8, 8 };
 		testMergeSort("test2", s2, t2);
-		int[] s3 = {1};
-		int[] t3 = {1};
+		int[] s3 = { 1 };
+		int[] t3 = { 1 };
 		testMergeSort("test3", s3, t3);
-		int[] s4 = {3,3,4,3,3,3};
-		int[] t4 = {3,3,3,3,3,4};
+		int[] s4 = { 3, 3, 4, 3, 3, 3 };
+		int[] t4 = { 3, 3, 3, 3, 3, 4 };
 		testMergeSort("test4", s4, t4);
-		int[] s5 = {9,7,5,3,1};
-		int[] t5 = {1,3,5,7,9};
+		int[] s5 = { 9, 7, 5, 3, 1 };
+		int[] t5 = { 1, 3, 5, 7, 9 };
 		testMergeSort("test5", s5, t5);
 		int[] s6 = {};
 		int[] t6 = {};
 		testMergeSort("test6", s6, t6);
 	}
-
-
 }
--- COM2031_Lab3/src/lab03/SortedTableSearch.java
+++ COM2031_Lab3/src/lab03/SortedTableSearch.java
@@ -4,85 +4,57 @@ package lab03;
  * Sorted Table Search
  * 
  * COM2031: Divide and Conquer Labs
- * Steve Schneider
+ * 
  */
 
 public class SortedTableSearch {
 
-/**
- * 	
- * @param table a 2d table of integers with no repeats, where every row is increasing and every column is increasing
- * @param x value being searched for
- * @return 1 if x is in the table, and 0 otherwise
- */
-	
-	
 	public static int tableSearch(int[][] table, int x) {
-		if(table.length==0) {return 0;};
-		if(table[0].length==0) {return 0;};
+		if (table.length == 0 || table[0].length == 0) {
+			return 0;
+		}
 		return dac_TableSearch(table, 0, 0, table.length, table[0].length, x);
 	}
 
-	
-/**
- * 	
- * @param table  a 2d table of integers with no repeats, where every row is increasing and every column is increasing
- * @param i (i,j) is the top left corner of the section to be searched
- * @param j
- * @param k (k,l) is the bottom right corner of the section to be searched
- * @param l
- * @param x value to be searched 
- * @return 1 if x is in the table between (i,j) (inclusive) and (k,l) (exclusive), otherwise return 0
- */
-
 	private static int dac_TableSearch(int[][] table, int i, int j, int k, int l, int x) {
-		if // BASE CASE
-			// TODO
-		else {
-			//DIVIDE
-			// TODO
-			//
-			//CONQUER
-			// TODO
-			//
-			//COMBINE
-			// TODO
+		// BASE CASE: if bounds are invalid
+		if (i >= k || j >= l) {
+			return 0;
 		}
-		
-	}
-	
-	/*
-	 * 
-	 * Utilities for testing
-	 * 
-	 */
 
-	
-	
-	
-	public static void testSearch(String name, int[][] table, int x, int expected){
-	    int n = tableSearch(table, x);
-	    if(n==expected) {
-	    	System.out.print(name+"  Pass");
-	    }
-	    if(n!=expected) {
-	    	System.out.println("*********************");	    	
-	    	System.out.print(name+"  Fail:  ");
-	    }
-	    if(n!=expected) {
-	    	System.out.print("correct output is "+expected+"\n");
-	    	System.out.print("*********************");
-	    }
-	    System.out.print("\n");
+		// Divide: get the middle element
+		int midValue = table[i][j];
+
+		if (midValue == x) {
+			return 1; // found
+		} else if (midValue > x) {
+			// If midValue is greater than x, eliminate the current column
+			return dac_TableSearch(table, i, j, k, j, x); // Search in the same row, left of midValue
+		} else {
+			// If midValue is less than x, eliminate the current row
+			return dac_TableSearch(table, i + 1, j, k, l, x); // Search in the same column, below midValue
 		}
-			
-	
+	}
+
+	public static void testSearch(String name, int[][] table, int x, int expected) {
+		int n = tableSearch(table, x);
+		if (n == expected) {
+			System.out.print(name + "  Pass");
+		} else {
+			System.out.println("*********************");
+			System.out.print(name + "  Fail:  ");
+			System.out.print("correct output is " + expected + "\n");
+			System.out.print("*********************");
+		}
+		System.out.print("\n");
+	}
+
 	public static void main(String[] args) {
 		int[][] table1 = {
-				{1,5,9},
-				{2,6,10},
-				{3,7,11}
-				};
+				{ 1, 5, 9 },
+				{ 2, 6, 10 },
+				{ 3, 7, 11 }
+		};
 		testSearch("test1", table1, 11, 1);
 		testSearch("test2", table1, 1, 1);
 		testSearch("test3", table1, 2, 1);
@@ -93,25 +65,17 @@ public class SortedTableSearch {
 		testSearch("test8", table1, 66, 0);
 		testSearch("test9", table1, 8, 0);
 		int[][] table2 = {
-				{1,5,9,15},
-				{2,6,10,18},
-				{3,7,11,21},
-				{4,8,22,24}};
+				{ 1, 5, 9, 15 },
+				{ 2, 6, 10, 18 },
+				{ 3, 7, 11, 21 },
+				{ 4, 8, 22, 24 } };
 		testSearch("test10", table2, 18, 1);
 		testSearch("test11", table2, 22, 1);
 		testSearch("test12", table2, 17, 0);
 		testSearch("test13", table2, 0, 0);
-		int[][] table3 = {{}};
+		int[][] table3 = { {} };
 		testSearch("test14", table3, 3, 0);
 		int[][] table4 = {};
 		testSearch("test15", table4, 3, 0);
-		
-		
-		
 	}
-
-	
-	}
-
-
-
+}
